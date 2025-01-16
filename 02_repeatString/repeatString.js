0ab26cf2a7ae844aedabc89e6179b93cdab0551d
Jayden Nawotka <156985453+qubered@users.noreply.github.com>
2025-01-16 17:31:42 +1100

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string,amount) {
+    let complete = "";
+    if (amount < 0) {
+        complete = "ERROR";
+    }
+    else {
+        for (let i=0; i<amount; i++) {
+            complete = complete.concat(string)
+        };
+    }
+    return complete;
 
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
