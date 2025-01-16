5af46193377ab89a70fe4f7b3ec8e53a73a9e52f
Jayden Nawotka <156985453+qubered@users.noreply.github.com>
2025-01-16 22:45:04 +1100

    Complete Exercide 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
+const sumAll = function(min,max) {
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+    if (min < 0 || max < 0) return "ERROR";
+    if (min > max) {
+        const tempNum = min;
+        min = max;
+        max = tempNum;
+    };
 
+    let calc = 0;
+    for (let i = min; i <= max; i++) {
+        calc+=i;
+    }
+    return calc;
 };
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
