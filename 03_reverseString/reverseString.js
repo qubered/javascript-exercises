d2e6ffb2e314ded54c0e83e9a58bb4d321f46d79
Jayden Nawotka <156985453+qubered@users.noreply.github.com>
2025-01-16 17:47:47 +1100

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringArray = [...string];
+    let stringArrayLen = stringArray.length;
+    let reversedString = "";
+    for (let i=0; i<stringArrayLen; i++) {
+        let poppedLetter = stringArray.pop();
+        reversedString += poppedLetter;
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
