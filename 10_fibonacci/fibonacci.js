6de183eb8b1594c24e25cc6a943e6c769da04291
Jayden Nawotka <156985453+qubered@users.noreply.github.com>
2025-01-25 18:22:23 +1100

    Complete exercise 10
    I am still confused by math.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num==NaN || num < 0) {
+        return "OOPS"
+    } else if (num == 0) {
+        return 0;
+    };
+    let a = 1;
+    let b = 0;
+    for (let i=2; i<=num; i++) {
+        let current = a+b;
+        b=a;
+        a=current;
+    }
+    return a;
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
