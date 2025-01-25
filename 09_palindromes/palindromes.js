cb3b5b272d4547917bf98220f4ff938410684d7f
Jayden Nawotka <156985453+qubered@users.noreply.github.com>
2025-01-25 17:53:12 +1100

    Complete exercise 9 kinda
    I did not really do it properly i forgot we are learning about
    arrays and kinda went rougue lol

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let cleanWord = word
+                        .replace(/[\s!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '')
+                        .toLowerCase();
+    let reversedWord = cleanWord
+                            .split("")
+                            .reverse()
+    return reversedWord == cleanWord;
 };
 
 // Do not edit below this line
