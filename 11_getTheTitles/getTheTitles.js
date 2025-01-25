6bb058cda5c90721cee9bc082d58d3fc269514e1
Jayden Nawotka <156985453+qubered@users.noreply.github.com>
2025-01-25 18:28:10 +1100

    Complete exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(bookArray) {
+    titles = []
+    bookArray.map(book => {
+        titles.push(book.title)
+    })
+    return titles
 };
 
 // Do not edit below this line
